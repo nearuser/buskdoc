49daaefae53bf718ac49353f685fdb3441cddc96
nearuser <usuarionoautorizado@gmail.com>
2025-03-10 02:47:02 -0300

    creacion del proyecto en vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <h1>app para buscar documentos libres de derecho de autor</h1>
+    </div>
+  )
+}
+
+export default App
